52e4d678d54f7578c66651cd5757331ad1f24f7b
M. J. Fromberger <fromberger@tailscale.com>
2024-08-20 14:12:57 -0700

    cmd/go-cache-plugin: implement the GOCACHEPROG protocol
    Add a tool that speaks the GOCACHEPROG protocol to serve a build cache to the
    Go toolchain, using a local directory backed by an S3 bucket.

---
+++ cmd/go-cache-plugin/commands.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"os"
+
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/creachadair/command"
+	"github.com/creachadair/gocache"
+	"github.com/creachadair/gocache/cachedir"
+	"github.com/tailscale/go-cache-plugin/s3cache"
+)
+
+// runLocal runs a cache locally communicating on stdin/stdout, for use as a
+// direct GOCACHEPROG plugin.
+func runLocal(env *command.Env) error {
+	switch {
+	case flags.CacheDir == "":
+		return env.Usagef("You must provide a --cache-dir")
+	case flags.S3Bucket == "":
+		return env.Usagef("You must provide an S3 --bucket name")
+	}
+	region, err := getBucketRegion(env.Context(), flags.S3Bucket)
+	if err != nil {
+		return env.Usagef("You must provide an S3 --region name")
+	}
+
+	dir, err := cachedir.New(flags.CacheDir)
+	if err != nil {
+		return fmt.Errorf("create local cache: %w", err)
+	}
+
+	cfg, err := config.LoadDefaultConfig(env.Context(), config.WithRegion(region))
+	if err != nil {
+		return fmt.Errorf("laod AWS config: %w", err)
+	}
+
+	vprintf("local cache directory: %s", flags.CacheDir)
+	vprintf("S3 cache bucket %q (%s)", flags.S3Bucket, region)
+	cache := &s3cache.Cache{
+		Local:             dir,
+		S3Client:          s3.NewFromConfig(cfg),
+		S3Bucket:          flags.S3Bucket,
+		KeyPrefix:         flags.KeyPrefix,
+		MinUploadSize:     flags.MinUploadSize,
+		UploadConcurrency: flags.UploadConcurrency,
+	}
+	close := cache.Close
+	if flags.Expiration > 0 {
+		dirClose := dir.Cleanup(flags.Expiration)
+		close = func(ctx context.Context) error {
+			return errors.Join(cache.Close(ctx), dirClose(ctx))
+		}
+	}
+	s := &gocache.Server{
+		Get:         cache.Get,
+		Put:         cache.Put,
+		Close:       close,
+		SetMetrics:  cache.SetMetrics,
+		MaxRequests: flags.Concurrency,
+		Logf:        vprintf,
+	}
+	if err := s.Run(env.Context(), os.Stdin, os.Stdout); err != nil {
+		return fmt.Errorf("cache server exited with error: %w", err)
+	}
+	if flags.Verbose {
+		fmt.Fprintln(os.Stderr, s.Metrics())
+	}
+	return nil
+}
---
+++ cmd/go-cache-plugin/go-cache-plugin.go
@@ -0,0 +1,112 @@
+// Program gocache implements the experimental GOCACHEPROG protocol over an S3
+// bucket, for use in builder and CI workers.
+package main
+
+import (
+	"cmp"
+	"context"
+	"flag"
+	"log"
+	"os"
+	"time"
+
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/creachadair/command"
+	"github.com/creachadair/flax"
+)
+
+var flags struct {
+	CacheDir          string        `flag:"cache-dir,default=$GOCACHE_DIR,Local cache directory (required)"`
+	S3Bucket          string        `flag:"bucket,default=$GOCACHE_S3_BUCKET,S3 bucket name (required)"`
+	S3Region          string        `flag:"region,default=$GOCACHE_S3_REGION,S3 region (required)"`
+	KeyPrefix         string        `flag:"prefix,default=$GOCACHE_KEY_PREFIX,S3 key prefix (optional)"`
+	MinUploadSize     int64         `flag:"min-upload-size,default=$GOCACHE_MIN_SIZE,Minimum object size to upload to S3 (in bytes)"`
+	Concurrency       int           `flag:"c,default=$GOCACHE_CONCURRENCY,Maximum number of concurrent requests"`
+	UploadConcurrency int           `flag:"u,Maximum concurrency for upload to S3"`
+	Expiration        time.Duration `flag:"x,default=$GOCACHE_EXPIRY,Cache expiration period (optional)"`
+	Verbose           bool          `flag:"v,default=$GOCACHE_VERBOSE,Enable verbose logging"`
+}
+
+func init() { flax.MustBind(flag.CommandLine, &flags) }
+
+func main() {
+	log.SetFlags(log.Ltime | log.Lmicroseconds)
+	root := &command.C{
+		Name:  command.ProgramName(),
+		Usage: "--cache-dir d --bucket b [options]\nhelp",
+		Help: `Run a cache service for the Go toolchain backed by an S3 bucket.
+
+This program serves the Go toolchain cache protocol on stdin/stdout.
+It is meant to be run by the "go" tool as a GOCACHEPROG plugin.
+For example:
+
+    GOCACHEPROG=` + command.ProgramName() + ` go build ./...
+
+Note that this requires a Go toolchain built with GOEXPERIMENT=cacheprog.
+
+You must provide --cache-dir, --bucket, and --region, or the corresponding
+environment variables (see "help environment").  Entries in the cache are
+stored in the specified S3 bucket, and staged in a local directory specified by
+the --cache-dir flag or GOCACHE_DIR environment.`,
+
+		SetFlags: command.Flags(flax.MustBind, &flags),
+		Run:      command.Adapt(runLocal),
+
+		Commands: []*command.C{
+			command.HelpCommand([]command.HelpTopic{{
+				Name: "environment",
+				Help: `Environment variables understood by this program.
+
+To make it easier to configure this tool for multiple workflows, most of the
+settings can be set via environment variables as well as flags.
+
+    Flag              Variable             Format    Example
+    --cache-dir       GOCACHE_DIR          string    "/tmp/xyzzy"
+    --bucket          GOCACHE_S3_BUCKET    string    "tailscale-build-cache"
+    --region          GOCACHE_S3_REGION    string    "us-east-1"
+    --prefix          GOCACHE_KEY_PREFIX   string    "worker-1"
+    --min-upload-size GOCACHE_MIN_SIZE     int64     100
+    -c                GOCACHE_CONCURRENCY  int       8
+    -x                GOCACHE_EXPIRY       duration  24h
+    -v                GOCACHE_VERBOSE      bool      true
+`,
+			}}),
+			command.VersionCommand(),
+		},
+	}
+	command.RunOrFail(root.NewEnv(nil), os.Args[1:])
+}
+
+// getBucketRegion reports the specified region for the given bucket.
+// if the --region flag was set, that value is returned without error.
+// Otherwise, it queries the GetBucketLocation API.
+func getBucketRegion(ctx context.Context, bucket string) (string, error) {
+	if flags.S3Region != "" {
+		return flags.S3Region, nil
+	}
+
+	// The default AWS region, which we use for resolving the bucket location
+	// and also serves as the fallback if the API reports an empty region name.
+	// The API returns "" for buckets in this region for historical reasons.
+	const defaultRegion = "us-east-1"
+
+	cfg, err := config.LoadDefaultConfig(ctx, config.WithRegion(defaultRegion))
+	if err != nil {
+		return "", err
+	}
+	cli := s3.NewFromConfig(cfg)
+	loc, err := cli.GetBucketLocation(ctx, &s3.GetBucketLocationInput{Bucket: &bucket})
+	if err != nil {
+		return "", err
+	}
+	return cmp.Or(string(loc.LocationConstraint), defaultRegion), nil
+}
+
+// vprintf acts as log.Printf if the --verbose flag is set; otherwise it
+// discards its input.
+func vprintf(msg string, args ...any) {
+	if flags.Verbose {
+		log.Printf(msg, args...)
+	}
+}
