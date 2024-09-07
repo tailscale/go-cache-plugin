69941c004a29128564c1a8fae9e2e22f2e3558ef
M. J. Fromberger <fromberger@tailscale.com>
2024-09-06 23:49:27 -0700

    cmd/go-cache-plugin: write out signing cert when it can't be installed

--- cmd/go-cache-plugin/addca_default.go
+++ cmd/go-cache-plugin/addca_default.go
@@ -7,12 +7,20 @@ package main
 
 import (
 	"errors"
+	"log"
 
+	"github.com/creachadair/atomicfile"
 	"github.com/creachadair/command"
 	"github.com/creachadair/tlsutil"
 )
 
 func installSigningCert(env *command.Env, cert tlsutil.Certificate) error {
+	const certFile = "revproxy-ca.crt"
+	if err := atomicfile.WriteData(certFile, cert.CertPEM(), 0644); err != nil {
+		log.Printf("WARNING: Unable to write cert file: %v", err)
+	} else {
+		log.Printf("Wrote signing cert to %s", certFile)
+	}
 	// TODO(creachadair): Maybe crib some other cases from mkcert, if we need
 	// them, for example:
 	// https://github.com/FiloSottile/mkcert/blob/master/truststore_darwin.go
